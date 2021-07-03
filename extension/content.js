6cd2c70645f16fba9cd2aa9a6ccdb1d137ad74e1
DESKTOP-G05BF1U\x <peter.morris@xoserve.com>
2021-07-03 14:40:17 +0100

    Nearly support dev.azure.com

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,33 @@
 (function () {
 
   const $ = (selector, ctx = document) => [].slice.call(ctx.querySelectorAll(selector));
+  let parentElementSelectors = [];
+  let childElementSelector = '';
+  let diagramRegex = /(.+)/s;
+
+  if (/github\.com/i.test(window.location.href)) {
+    parentElementSelectors = ['[lang="mermaid"]', '.language-mermaid'];
+    childElementSelector = 'code';
+  }
+
+  if (/dev\.azure\.com/i.test(window.location.href)) {
+    parentElementSelectors = ['div.markdown-editor-preview.markdown-content>pre.hljs'];
+    childElementSelector = 'code';
+    diagramRegex = /^\s*:::\s+mermaid\s+(.*):::\s*$/s;
+  }
+
+  if (/bitbucket\.org/i.test(window.location.href)) {
+
+  }
 
   function setupChart(elem, code) {
+    const diagramRegexMatch = diagramRegex.exec(code);
+    if (diagramRegexMatch === null) {
+      return;
+    }
+
+    code = diagramRegexMatch[1];
+
     var source_name = elem.id;
 
     if (elem.id == "") {
@@ -14,62 +39,63 @@
     var mermaid_name = 'mermaid_' + source_name;
     let existingDiagrams = $(`#${mermaid_name}`);
     if (existingDiagrams.length > 0) {
+      console.log('Reuse existing diagram');
       existingDiagram = existingDiagrams[0];
       // existingDiagram.innerHTML = code;
     } else {
+      console.log('Use new diagram');
       // Create the element that will house the rendered diagram.
+      console.log('Code is ', code);
       elem.insertAdjacentHTML('afterend', `<div class="mermaid" id="${mermaid_name}">${code}</div>`);
-      elem.style.display = 'none';
       existingDiagram = $(`#${mermaid_name}`)[0];
 
       // Create an observer to track changes to the diagram code.
-      const observer = new MutationObserver(() => { processElement(elem) });
+      const observer = new MutationObserver(() => {
+        console.log('Mutated element', elem);
+        processElement(elem);
+      });
       observer.observe(elem, { characterData: true });
     }
 
     try {
-      code = code.replace(/</g, '&lt;')
-      code = code.replace(/>/g, '&gt;')
       // Generate or regenerate diagram if it is existing.
       window.mermaid.render(elem.id, code, () => {
         console.log('>> done with ', elem.id);
       });
     }
-    catch(error) {
+    catch (error) {
       console.error('>>> mermaid error', error);
-      // existingDiagram.style.display = 'none';
-      // elem.style.display = 'block';
+      console.error('Code', code);
     }
   };
 
   function processElement(elem) {
-    if (elem.attributes["lang"] !== null && elem.attributes['lang'] !== undefined) {
-      const codeElem = $('code', elem)[0];
-      const code = codeElem.innerHTML;
-      setupChart(elem, code);
-    } else {
-      const code = elem.innerHTML;
-      setupChart(elem, code);
+    console.log('processElement', elem);
+    console.log('childElementSelector', childElementSelector);
+    const childElements = $(childElementSelector, elem);
+    for (const childElement of childElements) {
+      setupChart(elem, childElement.innerText);
     }
-  };
+  }
 
-  function onElementInsert(event){
+  function onElementInsert(event) {
     // We are only interested in the diagrams that trigger the css animation
     // called "mermaidDiagramCodeInserted". This is determined by the file
     // "on_change_animations.css".
     if (event.animationName !== "mermaidDiagramCodeInserted") {
       return
     }
+    console.log('onElementInsert', event.target);
     processElement(event.target);
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    // Github
-    $('[lang="mermaid"]').forEach(processElement);
-    $('.language-mermaid').forEach(processElement);
+    for (const parentElementSelector of parentElementSelectors) {
+      $(parentElementSelector).forEach(processElement);
+    }
 
     // Bitbucket
-    $('.codehilite.language-mermaid').forEach(processElement);
+    //$('.codehilite.language-mermaid').forEach(processElement);
 
     // This catches diagrams that are added to the page after it is loaded.
     // This might include comments from other users.
