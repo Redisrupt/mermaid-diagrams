623f96dac7509e3e2aea34769e6dfff7b89577c5
DESKTOP-G05BF1U\x <peter.morris@xoserve.com>
2021-07-19 15:18:36 +0100

    Include all diagram types

--- extension/content.js
+++ extension/content.js
@@ -12,7 +12,7 @@
 	if (/dev\.azure\.com/i.test(window.location.href)) {
 		parentElementSelectors = ['div.markdown-editor-preview.markdown-content > pre.hljs'];
 		childElementSelector = 'code';
-		diagramRegex = /^\s*(graph|hello|flowchart .*)$/s;
+		diagramRegex = /^\s*((classDiagram|erDiagram|flowchart|journey|gannt|graph|pie|requirementDiagram|sequenceDiagram|stateDiagram) ?.*)$/s;
 	}
 
 	function setupChart(elem, code) {
